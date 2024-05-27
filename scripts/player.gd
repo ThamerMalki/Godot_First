e3e569811854fa82511a21cea02218b1ec67cf41
ThamerMalki <ThamerGDev@gmail.com>
2024-05-27 22:58:56 +0300

    changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -22,7 +22,9 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		$AnimatedSprite2D.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.flip_h = false
 
 	move_and_slide()
