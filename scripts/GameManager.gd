3ef82aedad0613319c244f7b34f3e6e8faf5b301
ThamerMalki <ThamerGDev@gmail.com>
2024-05-31 00:51:07 +0300

    Added jump & coin SFX

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,7 +1,7 @@
 extends Node
-@onready var score_label = $"../Player/ScoreLabel"
+@onready var score_label_2 = $"../CanvasLayer/ScoreLabel2"
 
 var score =0
 func add_score():
 	score+=1
-	score_label.text = "Score:"+str(score)
+	score_label_2.text = "Score:"+str(score)
--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var jump_sfx = $JumpSFX
 
 
 func _physics_process(delta):
@@ -17,6 +18,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sfx.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
